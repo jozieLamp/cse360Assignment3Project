b0e9dce77cf9ce47f16eede31b265c58dc616133
jozieLamp <jalamp@asu.edu>
2016-02-19 11:58:34 -0700

    Initial version with calculator class and calculatorTest class.

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -1,35 +1,77 @@
 
 package cse360assign3;
 
-public class Calculator {
+/**
+ * Class to calculate simple number operations including addition, subtraction, multiplication
+ * and division of numbers.
+ * 
+ * @author Josephine Lamp, Pin 502 for CSE 360 Spring 2016
+ *
+ */
 
+public class Calculator 
+{
+
+	/** Private integer total variable to hold the total value */
 	private int total;
 	
-	public Calculator () {
+	/** Creates a calculator object, with default total of 0.*/
+	public Calculator () 
+	{
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {
+	/**
+	 * getTotal - returns the total computed by the calculator object
+	 * @return total value
+	 */
+	public int getTotal () 
+	{
 		return 0;
 	}
 	
-	public void add (int value) {
+	/**
+	 * add - add parameter to the total value
+	 * @param value number to be added
+	 */
+	public void add (int value) 
+	{
 		
 	}
 	
-	public void subtract (int value) {
+	/**
+	 * subtract - subtract the parameter from the total value
+	 * @param value number to be subtracted
+	 */
+	public void subtract (int value) 
+	{
 		
 	}
 	
-	public void multiply (int value) {
+	/**
+	 * multiply - multiply the parameter to the total value
+	 * @param value number to be multiplied
+	 */
+	public void multiply (int value) 
+	{
 		
 	}
 	
-	public void divide (int value) {
+	/**
+	 * divide - divide the total by the parameter
+	 * @param value value to divide the total by
+	 */
+	public void divide (int value) 
+	{
 		
 	}
 	
-	public String getHistory () {
+	/**
+	 * Return a string of the history of calculator changes to the total value
+	 * @return string of history of changes
+	 */
+	public String getHistory () 
+	{
 		return "";
 	}
 }
